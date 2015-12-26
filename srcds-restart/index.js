e80f53ab67d5e2ed9599906f1b20ebba815f6cc9
Swad <SwadicalRag@users.noreply.github.com>
2015-12-26 17:30:07 +1100

    Oh dear

--- srcds-restart/index.js
+++ srcds-restart/index.js
@@ -1,24 +1,28 @@
 var srcds = require("srcds-info");
 var exec = require("child_process").exec;
 
-client = srcds("127.0.0.1",27015,{
-    timeout: 2000
-});
-
 var errCount = 0;
 setInterval(function() {
-    client.info(function(err,info) {
+    client = srcds("www.swadical.net",27015,{
+        timeout: 9500
+    }).info(function(err,info) {
+        console.log(err,info);
         if(!err && info) {
             errCount = 0;
         }
         else {
             errCount++;
 
-            if(errCount > 3) {
+            if(errCount == 3) {
+                console.log("Restarted");
+                setTimeout(function() {
+                    errCount = 0;
+                },45000);
+
                 exec("./gmodserver stop",function(a,b,c) {
                     exec("./gmodserver restart");
                 });
             }
         }
     });
-},2500);
+},10000);
